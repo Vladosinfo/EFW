d7a49326c3ca02bf82ad5be4236bc5811910b97c
Vlad <vladosinfo@gmail.com>
2019-12-04 00:58:52 +0200

    Upload progect

---
+++ EFW/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkWeb.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public UserProfile Profile { get; set; }
+    }
+}
